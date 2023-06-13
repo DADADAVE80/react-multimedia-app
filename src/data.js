758eb217d552e5bbf2c8b9f74d34fdaa18fe6586
David Dada <daveproxy80@gmail.com>
2023-06-13 11:28:47 +0100

    cum-it

---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const data = [
+  {
+    id: 1,
+    name: "Video File 1",
+    type: "video",
+    path: "/file-server/video1.mp4",
+  },
+  {
+    id: 2,
+    name: "Video File 2",
+    type: "video",
+    path: "/file-server/video2.mp4",
+  },
+  {
+    id: 3,
+    name: "Audio File",
+    type: "audio",
+    path: "/file-server/audio.mp3",
+  },
+  {
+    id: 4,
+    name: "Document File",
+    type: "document",
+    path: "/file-server/document.pdf",
+  },
+  {
+    id: 5,
+    name: "Image File 1",
+    type: "image",
+    path: "/file-server/image1.png",
+  },
+  {
+    id: 6,
+    name: "Image File 2",
+    type: "image",
+    path: "/file-server/image2.png",
+  },
+];
